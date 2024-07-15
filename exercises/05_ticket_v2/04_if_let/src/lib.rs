b8970fa4926fc7cebbf8a8955ccd6fcf9cd6ca99
Mike Konkov <noospheratum@gmail.com>
2024-07-16 00:19:18 +0200

    broken

--- exercises/05_ticket_v2/04_if_let/src/lib.rs
+++ exercises/05_ticket_v2/04_if_let/src/lib.rs
@@ -8,7 +8,11 @@ impl Shape {
     // TODO: Implement the `radius` method using
     //  either an `if let` or a `let/else`.
     pub fn radius(&self) -> f64 {
-        todo!()
+        if let Shape::Circle { radius } = self {
+            *radius
+        } else {
+            panic!("Not a circle")
+        }
     }
 }
 
@@ -18,7 +22,7 @@ mod tests {
 
     #[test]
     fn test_circle() {
-        let _ = Shape::Circle { radius: 1.0 }.radius();
+    let _ = Shape::Circle { radius: 1.0 }.radius();
     }
 
     #[test]
--- exercises/05_ticket_v2/09_error_trait/src/lib.rs
+++ exercises/05_ticket_v2/09_error_trait/src/lib.rs
@@ -1,19 +1,35 @@
+use std::fmt::{Debug, Display, Formatter, Result};
+use std::error::Error;
 // TODO: Implement `Debug`, `Display` and `Error` for the `TicketNewError` enum.
 //  When implementing `Display`, you may want to use the `write!` macro from Rust's standard library.
 //  The docs for the `std::fmt` module are a good place to start and look for examples:
 //  https://doc.rust-lang.org/std/fmt/index.html#write
 
+#[derive(Debug)]
 enum TicketNewError {
     TitleError(String),
     DescriptionError(String),
 }
 
+impl Display for TicketNewError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), NewTicketError> {
+        write!(f, "{}", self)
+    }
+}
+
+impl Error for TicketNewError {}
 // TODO: `easy_ticket` should panic when the title is invalid, using the error message
 //   stored inside the relevant variant of the `TicketNewError` enum.
 //   When the description is invalid, instead, it should use a default description:
 //   "Description not provided".
 fn easy_ticket(title: String, description: String, status: Status) -> Ticket {
-    todo!()
+    match Ticket::new(title.clone(), description, status.clone()) {
+        Ok(ticket) => ticket,
+        Err(TicketNewError::DescriptionError(_)) => Ticket::new(title,
+                              "Description not provided".to_string(),
+                              status).unwrap(),
+        Err(TicketNewError::TitleError(msg)) => panic!(msg),
+    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
